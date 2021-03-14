04256af39675d1661859ef2e45c7ceaad3e6f732
unknown <denis_greben1991@mail.ru>
2021-03-14 12:16:46 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+
+    const numbers = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+
+    let numString = number.toString();
+
+    if (number === 0) return 'zero';
+
+    if (number < 20) {
+        return numbers[number];
+    }
+
+    if (number === 20 || number === 30 || number === 30 || number === 40 || number === 50 || number === 60 || number === 70 || number === 80 || number === 90) {
+        return tens[numString[0]];
+    }
+
+    if (numString.length === 2) {
+        return `${tens[numString[0]]} ${numbers[numString[1]]}`;
+    }
+
+    if (numString.length == 3) {
+        if (numString[1] === '0' && numString[2] === '0')
+            return `${numbers[numString[0]]} hundred`;
+        else
+            return `${numbers[numString[0]]} hundred ${toReadable(+(numString[1] + numString[2]))}`;
+    }
+
 }
